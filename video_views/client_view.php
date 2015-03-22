3128ba6535e644496efcdfaad9ec54704830ae1a
bradwhy <ar_brad@msn.com>
2015-03-23 08:15:12 +1000

    Update - last video version

--- video_views/client_view.php
+++ video_views/client_view.php
@@ -161,7 +161,7 @@ $name = "Surge Media - Video Dash";
 $frommail = "cs@videodash.surgehost.com.au";
 $mailto = 'video@surgemedia.com.au, webproduction@surgemedia.com.au'; // $cca_row['email'];
 $mailtoclient = 'video@surgemedia.com.au, webproduction@surgemedia.com.au'; // $cca_row['email'];
-$mailsubject = 'CLIENT #'.$last_video_under_project_row['version_num'].'' CHANGES – TO SURGE';
+$mailsubject = 'CLIENT #'.$last_video_under_project_row['version_num'].' CHANGES – TO SURGE';
 $headers = "MIME-Version: 1.0\r\n";
 $headers .= "Content-type: text/html; charset=utf-8\r\n";
 $headers .="From: ". $name . " <" . $frommail . ">\r\n";
