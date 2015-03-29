fc7a6a3107f09b355573f073006c03e6fe5ac782
Alex <alex.king@surgemedia.com.au>
2015-03-30 09:11:08 +1000

    Rename Your project for better consistency

--- video_views/client_header.php
+++ video_views/client_header.php
@@ -4,7 +4,7 @@
                         <nav>
                             <ul class="nav">
                                 <li><a href="javacript:void(0);"onclick="window.history.back();"><i class="fa fa-reply"></i> <span>Back</span></a></li>
-                                <li><a href="<? echo "http://".strtolower($_SERVER['SERVER_NAME'])."/c_projects_view.php";?>">My Projects</a></li>
+                                <li><a href="<? echo "http://".strtolower($_SERVER['SERVER_NAME'])."/c_projects_view.php";?>">Your Projects</a></li>
                                 <li><a href="<? echo "http://".strtolower($_SERVER['SERVER_NAME']);?>">logout</a></li>
                             </ul>
                         </nav>
