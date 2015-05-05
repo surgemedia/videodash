a6ab47cd85863b366f0864eb7c407c21057810e5
stagfoo <alexanderddking@gmail.com>
2015-05-06 09:14:14 +1000

    delete shit

--- js/jquery-loader.js
+++
@@ -1,6 +0,0 @@
-(function () {
-    // Get any jquery=___ param from the query string.
-    var jqversion = location.search.match(/[?&]jquery=(.*?)(?=&|$)/);
-
-    document.write("<script src='../libs/jquery" + jqversion[1] + "/dist/jquery.js'></script>");
-}());
