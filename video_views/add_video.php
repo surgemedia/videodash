7b82e873582c5d7666aa085b8c48fba435be7392
Vani <surge@Desktop-10s-iMac.local>
2015-08-04 10:27:46 +1000

    wrong email link

--- video_views/add_video.php
+++ video_views/add_video.php
@@ -118,7 +118,7 @@
                     <p>Dear '.$cca_row['contact_person'].'</p>
                     <p>We are pleased to inform you that your final video:['.$checksamelinkrow['project_name'].'] by Surge Media is ready for download.<br/>
                     You can download the final video from our Video Dash by clicking on the link below.<br/>
-                    <a href="http://http://videodash.surgehost.com.au/c_projects_view.php?email='.$cca_row['email'].'">Final Video</a><br/> 
+                    <a href="http://videodash.surgehost.com.au/c_projects_view.php?email='.$cca_row['email'].'">Final Video</a><br/> 
                     Let us take this opportunity to thank you for choosing <a href="http://surgemedia.com.au">Surge Media</a>. <br/>
                     We look forward to working with you again.</p>
                     ';
